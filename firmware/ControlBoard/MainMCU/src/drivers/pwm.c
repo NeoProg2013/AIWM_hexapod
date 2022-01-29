f33115ed3f1d33bac2e65357f97252e8e56ca7d6
NeoProg <NeoProg2013@gmail.com>
2022-01-30 00:38:42 +0300

    Small optimizations

--- firmware/ControlBoard/MainMCU/src/drivers/pwm.c
+++ firmware/ControlBoard/MainMCU/src/drivers/pwm.c
@@ -1,4 +1,4 @@
-/// ***************************************************************************
+    /// ***************************************************************************
 /// @file    pwm.c
 /// @author  NeoProg
 /// ***************************************************************************
@@ -23,9 +23,11 @@ typedef struct {
     uint32_t gpio_pin;
 } pwm_channel_t;
 
+// Active array of pointers to active buffer (sorted)
+// To this buffer can access from IRQ handler
+static pwm_channel_t* active_buffer_ptr[SUPPORT_PWM_CHANNELS_COUNT]; 
 
-static pwm_channel_t* active_buffer_ptr[SUPPORT_PWM_CHANNELS_COUNT]; // Array of pointers to active buffer. For fast sorting
-static pwm_channel_t active_buffer[SUPPORT_PWM_CHANNELS_COUNT];      // Mirror of shadow buffer
+static pwm_channel_t active_buffer[SUPPORT_PWM_CHANNELS_COUNT];      // PWM active buffer. It is copy of shadow buffer, but it has other RAM address
 static pwm_channel_t shadow_buffer[SUPPORT_PWM_CHANNELS_COUNT] = {   // Not sorted buffer. Using for write data from user.
     { .gpio_port = GPIOD, .gpio_pin =  8, .ticks = PWM_CHANNEL_DISABLE_VALUE },
     { .gpio_port = GPIOB, .gpio_pin = 15, .ticks = PWM_CHANNEL_DISABLE_VALUE },
@@ -54,6 +56,13 @@ static bool pwm_disable_is_requested = false;
 static uint32_t pwm_current_frequency = PWM_START_FREQUENCY_HZ;
 static uint32_t pwm_next_frequency = PWM_START_FREQUENCY_HZ;
 
+// Pre-calculated GPIOx BSR register values
+static uint16_t pwm_gpio_a_bsr = 0;
+static uint16_t pwm_gpio_b_bsr = 0;
+static uint16_t pwm_gpio_c_bsr = 0;
+static uint16_t pwm_gpio_d_bsr = 0;
+static uint16_t pwm_gpio_e_bsr = 0;
+
 uint64_t synchro = 0;
 
 
@@ -79,6 +88,12 @@ void pwm_init(uint32_t frequency) {
         gpio_set_output_type (active_buffer[i].gpio_port, active_buffer[i].gpio_pin, GPIO_TYPE_PUSH_PULL);
         gpio_set_output_speed(active_buffer[i].gpio_port, active_buffer[i].gpio_pin, GPIO_SPEED_HIGH);
         gpio_set_pull        (active_buffer[i].gpio_port, active_buffer[i].gpio_pin, GPIO_PULL_NO);
+        
+        if (active_buffer[i].gpio_port == GPIOA) pwm_gpio_a_bsr |= (0x01 << active_buffer[i].gpio_pin);
+        if (active_buffer[i].gpio_port == GPIOB) pwm_gpio_b_bsr |= (0x01 << active_buffer[i].gpio_pin);
+        if (active_buffer[i].gpio_port == GPIOC) pwm_gpio_c_bsr |= (0x01 << active_buffer[i].gpio_pin);
+        if (active_buffer[i].gpio_port == GPIOD) pwm_gpio_d_bsr |= (0x01 << active_buffer[i].gpio_pin);
+        if (active_buffer[i].gpio_port == GPIOE) pwm_gpio_e_bsr |= (0x01 << active_buffer[i].gpio_pin);
     }
 
     // Setup PWM timer: one pulse mode
@@ -165,6 +180,13 @@ void pwm_set_width(uint32_t channel, uint32_t width) {
 /// @param  none
 /// @return none
 /// ***************************************************************************
+int compare_channels(const void* ch1, const void* ch2) {
+    uint16_t ch1_ticks = ((pwm_channel_t*)ch1)->ticks;
+    uint16_t ch2_ticks = ((pwm_channel_t*)ch2)->ticks;
+    if (ch1_ticks > ch2_ticks) return  1;
+    if (ch1_ticks < ch2_ticks) return -1;
+    return 0;
+}
 void TIM17_IRQHandler(void) {
     static uint32_t ch_cursor = 0;
 
@@ -199,21 +221,15 @@ void TIM17_IRQHandler(void) {
             memcpy(active_buffer, shadow_buffer, sizeof(active_buffer));
         }
 
-        // Sorting PWM channels: bubble sorting method for time stable
-        for (uint32_t i = 0; i < SUPPORT_PWM_CHANNELS_COUNT - 1; ++i) {
-            for (uint32_t j = 0; j < SUPPORT_PWM_CHANNELS_COUNT - i - 1; ++j) {
-                if (active_buffer_ptr[j]->ticks > active_buffer_ptr[j + 1]->ticks) {
-                    pwm_channel_t* tmp = active_buffer_ptr[j];
-                    active_buffer_ptr[j] = active_buffer_ptr[j + 1];
-                    active_buffer_ptr[j + 1] = tmp;
-                }
-            }
-        }
+        // Sorting PWM channels
+        qsort(active_buffer_ptr, SUPPORT_PWM_CHANNELS_COUNT, sizeof(active_buffer_ptr[0]), compare_channels);
 
         // Set HIGH level for PWM outputs
-        for (uint32_t i = 0; i < SUPPORT_PWM_CHANNELS_COUNT; ++i) {
-            gpio_set(active_buffer_ptr[i]->gpio_port, active_buffer_ptr[i]->gpio_pin);
-        }
+        GPIOA->BSRR = pwm_gpio_a_bsr;
+        GPIOB->BSRR = pwm_gpio_b_bsr;
+        GPIOC->BSRR = pwm_gpio_c_bsr;
+        GPIOD->BSRR = pwm_gpio_d_bsr;
+        GPIOE->BSRR = pwm_gpio_e_bsr;
         
         // Reset and enable PWM timer
         ch_cursor = 0;
